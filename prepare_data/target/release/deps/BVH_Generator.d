1a2c0d695bcf094d546a4c1e8d8f433565f0b1f8
Snowplou <snowplou01@gmail.com>
2024-07-19 16:30:48 -0700

    now renders teapot

--- prepare_data/target/release/deps/BVH_Generator.d
+++ prepare_data/target/release/deps/BVH_Generator.d
@@ -1,10 +1,10 @@
-C:\Users\jacob\Documents\VSC\Ray Tracing\prepare_data\target\release\deps\BVH_Generator.exe: src/main.rs src\vector.rs src\triangle.rs src\bounding_box.rs src\node.rs src\../../objects/knight.bin
+C:\Users\jacob\Documents\VSC\Ray Tracing\prepare_data\target\release\deps\BVH_Generator.exe: src/main.rs src\vector.rs src\triangle.rs src\bounding_box.rs src\node.rs src\../../objects/teapot.bin
 
-C:\Users\jacob\Documents\VSC\Ray Tracing\prepare_data\target\release\deps\BVH_Generator.d: src/main.rs src\vector.rs src\triangle.rs src\bounding_box.rs src\node.rs src\../../objects/knight.bin
+C:\Users\jacob\Documents\VSC\Ray Tracing\prepare_data\target\release\deps\BVH_Generator.d: src/main.rs src\vector.rs src\triangle.rs src\bounding_box.rs src\node.rs src\../../objects/teapot.bin
 
 src/main.rs:
 src\vector.rs:
 src\triangle.rs:
 src\bounding_box.rs:
 src\node.rs:
-src\../../objects/knight.bin:
+src\../../objects/teapot.bin:
